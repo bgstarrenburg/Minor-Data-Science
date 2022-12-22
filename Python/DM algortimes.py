8b4fc78e44365ef24d4064f54017a332d96e8e03
NielsVLD <nielskrommenhoek@hotmail.nl>
2022-12-22 15:56:27 +0100

    Update DM algortimes.py

--- Python/DM algortimes.py
+++ Python/DM algortimes.py
@@ -1,4 +1,4 @@
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, Lasso, LassoCV
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from scipy import stats
 from sklearn.preprocessing import StandardScaler 
@@ -26,6 +26,48 @@ bigX = pd.DataFrame()
 bigY = pd.DataFrame()
 stateName = ""
 
+
+def Lasso_Regresion():
+    # Data to use
+    object= StandardScaler()
+    columnsToUse = ['Population', 'Federal Funding']
+    bigX = pd.DataFrame()
+    bigY = pd.DataFrame()
+    stateName = ""
+    for state in allStates:
+        stateData = allDataByStates[state]
+        sX = stateData.drop("Amount of Homeless People", axis=1)
+        sY = stateData["Amount of Homeless People"]
+        bigX = pd.concat([bigX, sX], axis=0)
+        bigY = pd.concat([bigY, sY], axis=0)
+
+    
+    # Test case
+    X_train, X_test, y_train, y_test = train_test_split(bigX, bigY, test_size=0.3, random_state=42)
+
+    # instance of lasso
+    lasso = Lasso(alpha=1.0,fit_intercept=False, tol=0.00000000000001,
+          max_iter=10000000000000, positive=True )
+    
+    lasso.fit(X_train, y_train)
+
+    lasso.score(X_test, y_test), lasso.score(X_train, y_train)
+
+    # Predictions
+
+    # predictions = lasso.predict(X_test)
+    # print(classification_report(y_test, predictions))
+    # print(f"{stateName}: ")
+    # print(confusion_matrix(y_test, predictions))
+    # print(accuracy_score(y_test, predictions))
+
+Lasso_Regresion()
+
+
+
+
+
+
 def Logistic_Regression():
     for state in ["CA"]:
         stateName = state
@@ -49,14 +91,24 @@ def Logistic_Regression():
 
 
 def K_neigbour():
+    columnsToUse = ['Population', 'Federal Funding']
+    bigX = pd.DataFrame()
+    bigY = pd.DataFrame()
+    stateName = ""
     for state in allStates:
-        stateName = state
         stateData = allDataByStates[state]
-        bigX = stateData.drop('Amount of Homeless People', axis=1)
-        bigY= stateData['Amount of Homeless People']
+        sX = stateData.drop("Amount of Homeless People", axis=1)
+        sY = stateData["Amount of Homeless People"]
+        bigX = pd.concat([bigX, sX], axis=0)
+        bigY = pd.concat([bigY, sY], axis=0)
+    # for state in allStates:
+    #     stateName = state
+    #     stateData = allDataByStates[state]
+    #     bigX = stateData.drop('Amount of Homeless People', axis=1)
+    #     bigY= stateData['Amount of Homeless People']
 
     # Split dataset into random train and test subsets:
-    X_train, X_test, y_train, y_test = train_test_split(bigX, bigY, test_size=0.20, random_state=42) 
+    X_train, X_test, y_train, y_test = train_test_split(bigX, bigY, test_size=0.30, random_state=42) 
 
     # Standardize features by removing mean and scaling to unit variance:
     scaler = StandardScaler()
@@ -78,4 +130,4 @@ def K_neigbour():
     print(f"{stateName}")
 
 
-K_neigbour()
+# K_neigbour()
